91cf4f4a7f0d92486d291676ef90d3bbba33130c
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-07-03 10:40:30 +0300

    add sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,160 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 
-	
+
+	// tabs На головній 
+
+	const targetList = document.querySelector('.target__tabs');
+	if(targetList){
+	  const allContentBlocks = Array.from(document.querySelectorAll('.target__content'));
+	  const alltargetLinks = Array.from(document.querySelectorAll('.target__list-item'));
+	  
+	  let currentIndex = 0;
+	  let frontBlockId = "target-1";
+	  const tabsLinks = document.querySelectorAll('.target__list-item');
+	  function addTabsActive () {
+		  tabsLinks.forEach((button, index) => {
+			button.addEventListener('click', () => {
+			  tabsLinks.forEach((otherButton) => {
+				otherButton.classList.remove('active');
+			  });
+			  button.classList.add('active');
+			  showContent(button.dataset.name, index);
+			});
+		  });
+		}
+	  addTabsActive ();
+	  function updateActiveTab(index) { 
+		  tabsLinks.forEach((button, i) => {
+			  if (i === index) {
+				  button.classList.add('active'); 
+			  } else {
+				  button.classList.remove('active'); 
+			  }
+		  });
+	  }
+	  
+	  function changeSlide(blockId) {
+		  allContentBlocks.forEach((block, index) => {
+			  if (block.getAttribute('id') === blockId) {
+				  block.style.display = 'flex';
+				  block.style.opacity = 1;
+				  currentIndex = index;
+			  } else {
+				  block.style.opacity = 0;
+				  block.style.display = 'none';
+			  }
+		  });
+		  frontBlockId = blockId;
+	  }
+	  function showContent(itemName, index) {
+        changeSlide(itemName, index);
+        updateActiveTab(index);
+    	}
+    addTabsActive();
+    showContent(frontBlockId, 0);
+	}
+
+	// слайдер для відгуків
+
+	const reviewsSliderInit = document.querySelector('.reviewsSlider');
+	if (reviewsSliderInit){
+		const reviewsSlide = document.querySelectorAll('.reviews-slide');
+		const reviewsArrows = document.querySelector('.reviews-arrows');
+		if(reviewsSlide.length < 2){
+			reviewsArrows.style.display = 'none';
+		} else{
+			reviewsArrows.style.display = 'flex';
+		}
+		const reviewsSwiper = new Swiper(".reviewsSlider", {
+			pagination: {
+			  el: ".reviews-pagination",
+			  clickable: true,
+			},
+			navigation: {
+			nextEl: '.reviews-button-next',
+			prevEl: '.reviews-button-prev',
+			},
+				slidesPerView: 1,
+			spaceBetween: 10,
+			breakpoints: {
+			767: {
+			slidesPerView: 2,
+			spaceBetween: 20
+			},
+			1023: {
+			slidesPerView: 3,
+			spaceBetween: 20
+			}
+		},
+		  });
+	}
+	// слайдер для новин з блогу
+
+	const newsSliderInit = document.querySelector('.newsSlider');
+	if (newsSliderInit){
+		const newsSlide = document.querySelectorAll('.news-slide');
+		const newsArrows = document.querySelector('.news-arrows');
+		if(newsSlide.length < 2){
+			newsArrows.style.display = 'none';
+		} else{
+			newsArrows.style.display = 'flex';
+		}
+		const newsSwiper = new Swiper(".newsSlider", {
+			pagination: {
+			  el: ".news-pagination",
+			  clickable: true,
+			},
+			navigation: {
+			nextEl: '.news-button-next',
+			prevEl: '.news-button-prev',
+			},
+				slidesPerView: 1,
+			spaceBetween: 10,
+			breakpoints: {
+			767: {
+			slidesPerView: 2,
+			spaceBetween: 20
+			},
+			1023: {
+			slidesPerView: 3,
+			spaceBetween: 20
+			}
+		},
+		  });
+	}
+	// для кнопки читати більше в статті
+	const articleContainer = document.querySelector("#article");
+
+	if (articleContainer) {
+		const createBtn = document.createElement('button');
+		createBtn.classList.add('article-btn');
+		createBtn.innerText = 'Read more'; 
+		articleContainer.appendChild(createBtn);
+
+		const articleContent = document.querySelectorAll('.state__article');
+
+		createBtn.addEventListener("click", function(e) {
+			e.preventDefault();
+			articleContent.forEach(function(content) {
+				if (createBtn.innerText === 'Read Less') {
+					content.style.height = "482px";
+					createBtn.innerText = 'Read more';
+				} else {
+					content.style.height = "auto";
+					createBtn.innerText = 'Read Less';
+				}
+			});
+		});
+
+		articleContent.forEach(function(content) {
+			if (content.offsetHeight >= 482) {
+				content.style.height = "482px";
+				content.style.overflow = 'hidden';
+			}
+		});
+	}
+
 
 	
 
