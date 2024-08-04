9f4b033e2cfb5a055c02d124131753d683f3a7fa
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-08-04 12:42:54 +0300

    fix footer and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
 //функція прелоадера
 document.addEventListener("DOMContentLoaded", function () {
-  let progressBar = document.querySelector(".preloader__progress");
+	playPreload();
+	navLinksCloseMenu();
+	openResizeMenu();
+	animateSectionPosition();
+	smoothScrollOnPage();
+	openTabs();
+	reviewsSlider();
+	openNewsSlider();
+	articleOpenBtn();
+	accordionFunction();
+	updateStickyElement();
+	showDate();
+	reviewsFormChecked(); //там на укр.яз настроенно все
+	showPopUpBook();
+	showPopUpContact();
+}); //кінець DOMContentLoaded
+
+let lastScrollTop = 0;
+const windowInnerWidth = window.innerWidth;
+const headerNavigation = document.querySelector(".header__navigation");
+
+//функція для анімації  прелоадеру
+
+const playPreload = () =>{
+	const preload = document.querySelector(".preloader");
+	if(!preload) return;
+
+	let progressBar = document.querySelector(".preloader__progress");
   let progressAnimationImg = document.querySelector(".preloader__wrap img");
   let progressValue = 0;
   let interval = setInterval(increaseProgress, 20);
@@ -14,114 +41,126 @@ document.addEventListener("DOMContentLoaded", function () {
     if (progressValue >= 100) {
       clearInterval(interval);
       document.querySelector(".preloader").style.display = "none";
-      document.documentElement.style.overflow = "visible";
+      document.documentElement.classList.add("visible");
     }
   }
+}
+//функція для закриття меню при переході за посиланнями
 
-  //функція для меню та того аби воно динамічно змінювало висоту
-  const navLinks = document.querySelectorAll("nav a");
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      htmlElement.classList.remove("open");
-    });
-  });
+  const navLinksCloseMenu = () =>{
+		const navLinks = document.querySelectorAll("nav a");
+		if(!navLinks) return;
+		navLinks.forEach((link) => {
+			link.addEventListener("click", () => {
+				htmlElement.classList.remove("open");
+			});
+		});
+	}
+ 
+	//функція для визначення висоти меню в залежності того чи відбулась зміна ширини вікна та необхідності його коррекції
+	const openResizeMenu = () =>{
+		
+		const resizeMenuBtn = document.querySelector(".resize-menu");
+		const htmlElement = document.querySelector("html");
+	
+		const headerTop = document.querySelector(".header__top");
+		const headerBottom = document.querySelector(".header__bottom");
+		const headerMobMenu = document.querySelector(".header__mob-menu");
+	
+		function getVisibleHeight(el) {
+			const rect = el.getBoundingClientRect();
+			const windowHeight =
+				window.innerHeight || document.documentElement.clientHeight;
+	
+			const topVisible = Math.max(rect.top, 0);
+			const bottomVisible = Math.min(rect.bottom, windowHeight);
+			const visibleHeight = bottomVisible - topVisible;
+			return Math.max(0, Math.min(visibleHeight, rect.height));
+		}
+	
+		function updateMenuPosition(scrollTop) {
+			const headerTopHeight = getVisibleHeight(headerTop);
+			const headerBottomHeight = getVisibleHeight(headerBottom);
+			const headerNavigationHeight = headerNavigation.offsetHeight;
+	
+			if (htmlElement.classList.contains("open")) {
+				headerMobMenu.style.top = `${headerTopHeight + headerBottomHeight}px`;
+				headerMobMenu.style.height = `calc(100vh - ${
+					headerTopHeight + headerBottomHeight
+				}px)`;
+				if (headerTopHeight <= 0 && headerBottomHeight <= 0) {
+					headerMobMenu.style.top = `${headerNavigationHeight}px`;
+					headerMobMenu.style.height = `calc(100vh + ${headerNavigationHeight}px)`;
+				}
+			} else if (scrollTop <= 0) {
+				headerMobMenu.style.top = `- ${headerNavigationHeight}px`;
+				headerMobMenu.style.height = `calc(100vh + ${headerNavigationHeight}px)`;
+			}
+		}
+	
+		
+	
+		resizeMenuBtn.addEventListener("click", function () {
+			htmlElement.classList.toggle("open");
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			updateMenuPosition(scrollTop);
+		});
+	
+		window.addEventListener("load", function () {
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			updateMenuPosition(scrollTop);
+		});
+		window.addEventListener("scroll", function () {
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			if (windowInnerWidth >= 1024) {
+				if (scrollTop > lastScrollTop) {
+					if (scrollTop > 100) {
+						headerNavigation.classList.add("fixed-header-nav");
+						headerNavigation.style.animationName = "smoothScroll";
+					}
+				} else if (scrollTop <= 0) {
+					headerNavigation.classList.remove("fixed-header-nav");
+					headerNavigation.style.animationName = "removeSmoothScroll";
+				}
+				lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+				updateMenuPosition(scrollTop);
+			}
+		});
+	}
 
-  let lastScrollTop = 0;
-  const windowInnerWidth = window.innerWidth;
-  const resizeMenuBtn = document.querySelector(".resize-menu");
-  const htmlElement = document.querySelector("html");
-  const headerNavigation = document.querySelector(".header__navigation");
-  const headerTop = document.querySelector(".header__top");
-  const headerBottom = document.querySelector(".header__bottom");
-  const headerMobMenu = document.querySelector(".header__mob-menu");
 
-  function getVisibleHeight(el) {
-    const rect = el.getBoundingClientRect();
-    const windowHeight =
-      window.innerHeight || document.documentElement.clientHeight;
 
-    const topVisible = Math.max(rect.top, 0);
-    const bottomVisible = Math.min(rect.bottom, windowHeight);
-    const visibleHeight = bottomVisible - topVisible;
-    return Math.max(0, Math.min(visibleHeight, rect.height));
-  }
-
-  function updateMenuPosition(scrollTop) {
-    const headerTopHeight = getVisibleHeight(headerTop);
-    const headerBottomHeight = getVisibleHeight(headerBottom);
-    const headerNavigationHeight = headerNavigation.offsetHeight;
-
-    if (htmlElement.classList.contains("open")) {
-      headerMobMenu.style.top = `${headerTopHeight + headerBottomHeight}px`;
-      headerMobMenu.style.height = `calc(100vh - ${
-        headerTopHeight + headerBottomHeight
-      }px)`;
-      if (headerTopHeight <= 0 && headerBottomHeight <= 0) {
-        headerMobMenu.style.top = `${headerNavigationHeight}px`;
-        headerMobMenu.style.height = `calc(100vh + ${headerNavigationHeight}px)`;
-      }
-    } else if (scrollTop <= 0) {
-      headerMobMenu.style.top = `- ${headerNavigationHeight}px`;
-      headerMobMenu.style.height = `calc(100vh + ${headerNavigationHeight}px)`;
-    }
-  }
-
-  window.addEventListener("scroll", function () {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (windowInnerWidth >= 1024) {
-      if (scrollTop > lastScrollTop) {
-        if (scrollTop > 100) {
-          headerNavigation.classList.add("fixed-header-nav");
-          headerNavigation.style.animationName = "smoothScroll";
-        }
-      } else if (scrollTop <= 0) {
-        headerNavigation.classList.remove("fixed-header-nav");
-        headerNavigation.style.animationName = "removeSmoothScroll";
-      }
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-      updateMenuPosition(scrollTop);
-    }
-  });
-
-  resizeMenuBtn.addEventListener("click", function () {
-    htmlElement.classList.toggle("open");
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    updateMenuPosition(scrollTop);
-  });
-
-  window.addEventListener("load", function () {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    updateMenuPosition(scrollTop);
-  });
-
-  //анімація для секцій
-  const sections = document.querySelectorAll(".sectionScroll");
-
-  const options = {
-    root: document,
-    rootMargin: "0px",
-    threshold: 0.1,
-  };
-
-  const callback = function (entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting && !entry.target.classList.contains("animate")) {
-        entry.target.classList.add("animate");
-      } else if (
-        !entry.isIntersecting &&
-        entry.target.classList.contains("animate")
-      ) {
-        // Видалити клас .animate, якщо елемент виходить з області видимості
-        entry.target.classList.remove("animate");
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(callback, options);
-
-  sections.forEach((section) => observer.observe(section));
-
-  //гарний скрол по сторінці
+	//анімація позиції секції на сторінці
+  const animateSectionPosition = () =>{
+		const sections = document.querySelectorAll(".sectionScroll");
+		if(!sections) return;
+		const options = {
+			root: document,
+			rootMargin: "0px",
+			threshold: 0.1,
+		};
+	
+		const callback = function (entries, observer) {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting && !entry.target.classList.contains("animate")) {
+					entry.target.classList.add("animate");
+				} else if (
+					!entry.isIntersecting &&
+					entry.target.classList.contains("animate")
+				) {
+					entry.target.classList.remove("animate");
+				}
+			});
+		};
+	
+		const observer = new IntersectionObserver(callback, options);
+	
+		sections.forEach((section) => observer.observe(section));
+	
+	}
+ //гарний скрол по сторінці
+const smoothScrollOnPage = () =>{
+ 
   document.querySelectorAll('a[href^="#"').forEach((link) => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
@@ -140,19 +179,18 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+}
+
 
   // tabs На головній
+const openTabs = () =>{
+	const targetList = document.querySelectorAll(".target__tabs");
+  if (!targetList) return;
 
-  const targetList = document.querySelector(".target__tabs");
-  if (targetList) {
     const allContentBlocks = Array.from(
       document.querySelectorAll(".target__content")
     );
-    const alltargetLinks = Array.from(
-      document.querySelectorAll(".target__list-item")
-    );
 
-    let currentIndex = 0;
     let frontBlockId = "target-1";
     const tabsLinks = document.querySelectorAll(".target__list-item");
     function addTabsActive() {
@@ -198,10 +236,12 @@ document.addEventListener("DOMContentLoaded", function () {
     showContent(frontBlockId, 0);
   }
 
-  // слайдер для відгуків
 
-  const reviewsSliderInit = document.querySelector(".reviewsSlider");
-  if (reviewsSliderInit) {
+  
+  // слайдер для відгуків
+		const reviewsSlider = () =>{
+	const reviewsSliderInit = document.querySelector(".reviewsSlider");
+  if (!reviewsSliderInit)  return 
     const reviewsSlide = document.querySelectorAll(".reviews-slide");
     const reviewsArrows = document.querySelector(".reviews-arrows");
     if (reviewsSlide.length < 2 || innerWidth <= 767) {
@@ -232,10 +272,10 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     });
   }
-  // слайдер для новин з блогу
-
-  const newsSliderInit = document.querySelector(".newsSlider");
-  if (newsSliderInit) {
+// слайдер для новин з блогу
+	const openNewsSlider = () =>{
+		 const newsSliderInit = document.querySelector(".newsSlider");
+  if (!newsSliderInit) return;
     const newsSlide = document.querySelectorAll(".news-slide");
     const newsArrows = document.querySelector(".news-arrows");
     if (newsSlide.length < 2 || innerWidth <= 767) {
@@ -265,83 +305,94 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       },
     });
-  }
+  
+	}
+ 
   // для кнопки читати більше в статті
-  const articleContainer = document.querySelector("#article");
-
-  if (articleContainer) {
-    const createBtn = document.createElement("button");
-    createBtn.classList.add("article-btn");
-    createBtn.innerText = "Read more";
-    articleContainer.appendChild(createBtn);
-
-    const articleContent = document.querySelectorAll(".state__article");
-
-    createBtn.addEventListener("click", function (e) {
-      e.preventDefault();
-      articleContent.forEach(function (content) {
-        if (content.style.height >= 482) {
-          content.style.height = "482px";
-          createBtn.innerText = "Read more";
-        } else {
-          content.style.height = "auto";
-          createBtn.style.display = "none";
-        }
-      });
-    });
-
-    articleContent.forEach(function (content) {
-      if (content.offsetHeight >= 482) {
-        content.style.height = "482px";
-        content.style.overflow = "hidden";
-      }
-    });
-  }
-
+	const articleOpenBtn = () =>{
+		const articleContainer = document.querySelectorAll(".article");
+		if (!articleContainer) return;
+		articleContainer.forEach((container) =>{
+			const createBtn = document.createElement("button");
+			createBtn.classList.add("article-btn");
+			createBtn.innerText = "Read more";
+			container.appendChild(createBtn);
+	
+			const articleContent = container.querySelectorAll(".state__article");
+	
+			createBtn.addEventListener("click", function (e) {
+				e.preventDefault();
+				articleContent.forEach(function (content) {
+					if (content.style.height >= 482) {
+						content.style.height = "482px";
+						createBtn.innerText = "Read more";
+					} else {
+						content.style.height = "auto";
+						createBtn.style.display = "none";
+					}
+				});
+			});
+	
+			articleContent.forEach(function (content) {
+				if (content.offsetHeight >= 482) {
+					content.style.height = "482px";
+					content.style.overflow = "hidden";
+				}
+			});
+		})
+			
+		}
+	  
+const accordionFunction = () =>{
   const accordionItemsProduct = document.querySelectorAll(".accord-item");
-  if (accordionItemsProduct) {
+  if (!accordionItemsProduct) return
     accordionItemsProduct.forEach((item) => {
       item.addEventListener("click", function () {
         this.classList.toggle("active");
       });
     });
   }
+//для блоків гармошкою
+const updateStickyElement = () =>{
+ 
+ const stickyElement = document.querySelectorAll(".scrolling_item");
+ if(!stickyElement) return
+	 const resizeStickyElement = () => {
+		 if (windowInnerWidth >= 1024 && stickyElement) {
+			 stickyElement.forEach((sticky, index) => {
+				 sticky.style.top = `calc(100px + ${50 * index}px)`;
+			 });
+		 } else if (windowInnerWidth <= 1023 && stickyElement) {
+			 stickyElement.forEach((sticky, index) => {
+				 sticky.style.top = `calc(50px + ${50 * index}px)`;
+			 });
+		 }
+	 };
+	 resizeStickyElement();
+	 window.addEventListener("resize", resizeStickyElement);
+ }
 
-  //для блоків гармошкою
-  const stickyElement = document.querySelectorAll(".scrolling_item");
-  if(stickyElement){
-		const resizeStickyElement = () => {
-			if (windowInnerWidth >= 1024 && stickyElement) {
-				stickyElement.forEach((sticky, index) => {
-					sticky.style.top = `calc(100px + ${50 * index}px)`;
-				});
-			} else if (windowInnerWidth <= 1023 && stickyElement) {
-				stickyElement.forEach((sticky, index) => {
-					sticky.style.top = `calc(50px + ${50 * index}px)`;
-				});
-			}
-		};
-		resizeStickyElement();
-		window.addEventListener("resize", resizeStickyElement);
-	}
 
   // для інпуту з датою  аби встановлювалась поточна дата
-  dateInput = document.querySelector(".input_date");
-  if (dateInput) {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, "0");
-    var mm = String(today.getMonth() + 1).padStart(2, "0");
-    var yyyy = today.getFullYear();
-
-    today = yyyy + "-" + mm + "-" + dd;
-
-    dateInput.setAttribute("min", today);
-    dateInput.value = today;
-  }
-
+	const showDate = () =>{
+		dateInput = document.querySelector(".input_date");
+		if (!dateInput) return; 
+			const today = new Date();
+			const dd = String(today.getDate()).padStart(2, "0");
+			const mm = String(today.getMonth() + 1).padStart(2, "0");
+			const yyyy = today.getFullYear();
+	
+			today = yyyy + "-" + mm + "-" + dd;
+	
+			dateInput.setAttribute("min", today);
+			dateInput.value = today;
+		}
+	
+	
+ 
   //для перевірки форми з відгуками
-
-  if (document.querySelector(".reviewForm")) {
+const reviewsFormChecked = () =>{
+	if (document.querySelector(".reviewForm")) {
     const commentForm = document.getElementById("comment-form");
     const commentName = document.getElementById("name");
     const commentSurname = document.getElementById("lastName");
@@ -404,12 +455,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-
-  // функції для попапів
-  const popUpBook = document.querySelector(".popup-book");
-  const popupBg = document.querySelectorAll(".popup-bg");
+}
   // функція для попап, який з заповненням форми при натискані кнопки Book
-  if (popUpBook) {
+const showPopUpBook = () =>{
+	const popUpBook = document.querySelector(".popup-book");
+  const popupBg = document.querySelectorAll(".popup-bg");
+	if (!popUpBook) return 
     const closePopUpBook = document.querySelector(".popup-book-close");
     const popupButtons = document.querySelectorAll(
       '[data-button="popup-book"]'
@@ -429,27 +480,35 @@ document.addEventListener("DOMContentLoaded", function () {
       })
     );
   }
-  // функція попапу який з'являється через 30 секунд після заванатаження сторінки
-  const popUpContact = document.querySelector(".popup-contact");
-  if (popUpContact) {
-    const popupWrapper = document.querySelectorAll(".popup");
-    const closePopUpContact = document.querySelector(".popup-contact-close");
-    const popupContactButtons = document.querySelectorAll(
-      '[data-button="popup-contact"]'
-    );
 
-    popupContactButtons.forEach((popBtn) => {
-      popBtn.addEventListener("click", () => {
-        popUpContact.classList.add("open");
-      });
-    });
-    popupBg.forEach((bg) =>
-      bg.addEventListener("click", () => {
-        popUpContact.classList.remove("open");
-      })
-    );
-    closePopUpContact.addEventListener("click", () => {
-      popUpContact.classList.remove("open");
-    });
-  }
-}); //кінець DOMContentLoaded
+  // функції для попапів
+ 
+
+
+  
+ 
+  // функція попапу який з'являється через 30 секунд після заванатаження сторінки
+	const showPopUpContact = () =>{
+		const popUpContact = document.querySelector(".popup-contact");
+		if (!popUpContact) return
+			const popupWrapper = document.querySelectorAll(".popup");
+			const popupBg = document.querySelectorAll(".popup-bg");
+			const closePopUpContact = document.querySelector(".popup-contact-close");
+			const popupContactButtons = document.querySelectorAll(
+				'[data-button="popup-contact"]'
+			);
+			popupContactButtons.forEach((popBtn) => {
+				popBtn.addEventListener("click", () => {
+					popUpContact.classList.add("open");
+				});
+			});
+			popupBg.forEach((bg) =>
+				bg.addEventListener("click", () => {
+					popUpContact.classList.remove("open");
+				})
+			);
+			closePopUpContact.addEventListener("click", () => {
+				popUpContact.classList.remove("open");
+			});
+		}
+ 
